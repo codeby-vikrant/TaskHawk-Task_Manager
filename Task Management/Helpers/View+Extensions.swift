191762f55133d0dd8e3b93502f8f730e6c34daec
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-03 00:41:21 +0530

    created size extensions

--- Task Management/Helpers/View+Extensions.swift
+++ Task Management/Helpers/View+Extensions.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 
-struct View_Extensions: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension View{
+    @ViewBuilder
+    func hSpacing(_ alignment: Alignment) -> some View{
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
     }
-}
-
-#Preview {
-    View_Extensions()
+    
+    @ViewBuilder
+    func vSpacing(_ alignment: Alignment) -> some View{
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
+    }
 }
