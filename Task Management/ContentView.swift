a81ed0f6b161fc2943de930ebbc30552a4277a77
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-03 00:45:07 +0530

    made new file for home page with bg height width

--- Task Management/ContentView.swift
+++ Task Management/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Home_Page()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.BG)
+            .preferredColorScheme(.light)
+        
     }
 }
 
---
+++ Task Management/View/Home Page.swift
@@ -0,0 +1,18 @@
+//
+//  Home Page.swift
+//  Task Management
+//
+//  Created by Vikrant Vani on 03/08/24.
+//
+
+import SwiftUI
+
+struct Home_Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
