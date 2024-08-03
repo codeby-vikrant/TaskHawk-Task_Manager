1d8889995c0db3eacbacb82aab7ad3586e2ee5ad
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-03 15:01:26 +0530

    made new file offsetkey

---
+++ Task Management/Helpers/OffsetKey.swift
@@ -0,0 +1,15 @@
+//
+//  OffsetKey.swift
+//  Task Management
+//
+//  Created by Vikrant Vani on 03/08/24.
+//
+
+import SwiftUI
+
+struct OffsetKey: PreferenceKey{
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
