c720af8c9c0aac6db8a484446acf0750e9964d6e
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-08-03 11:18:19 +0530

    added moth and year on screen

--- Task Management/View/Home Page.swift
+++ Task Management/View/Home Page.swift
@@ -11,8 +11,21 @@ struct Home_Page: View {
     @State private var currentDate: Date = .init()
     var body: some View {
         VStack(alignment: .leading, spacing: 0, content: {
-            
+           HeaderView()
         })
+    }
+    @ViewBuilder
+    func HeaderView() -> some View{
+        VStack(alignment: .leading, spacing: 6){
+            HStack(spacing: 5){
+                Text(currentDate.format("MMMM"))
+                    .foregroundStyle(.darkBlue)
+                
+                Text(currentDate.format("YYYY"))
+                    .foregroundStyle(.gray)
+            }
+            .font(.title.bold())
+        }
     }
 }
 
